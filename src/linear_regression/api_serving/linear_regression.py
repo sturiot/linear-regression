06d0cc97cc3157199e97adc483d3ef926c7ba86e
sturiot <sturiot@outlook.fr>
2022-12-29 10:28:55 +0100

    prepare for the custom seldon kedro server

--- src/linear_regression/api_serving/linear_regression.py
+++ src/linear_regression/api_serving/linear_regression.py
@@ -1,6 +1,8 @@
 import logging
 import json
 import pickle
+import seldon_core
+from seldon_core.user_model import SeldonComponent
 from kedro.framework.project import configure_project
 from kedro.framework.session import KedroSession
 from typing import Any, Dict
@@ -8,23 +10,50 @@ from typing import Any, Dict
 logger = logging.getLogger(__name__)
 
 
-class LinearRegression(object):
+class LinearRegression(SeldonComponent):
     """
     Model template. You can load your model parameters in __init__ from a location accessible at runtime
     """
 
-    def __init__(self):
+    def __init__(self, env: str = None):
         """
         Add any initialization parameters. These will be passed at runtime from the graph definition parameters defined in your seldondeployment kubernetes resource manifest.
         """
-
-
         logger.info("Initializing")
-        project_name = 'linear_regression'
-        configure_project(project_name)
-        with KedroSession.create() as session:
-            self._model = session.run(pipeline_name='predict', node_names=['load_model'])      
+        self.project_name = 'linear_regression'
+        self.pipeline_name = 'predict'
+        self.env = env
+        configure_project(self.project_name)
+        self._load_model()
         logger.info("Initialized")
+
+    def _load_model(self):
+        with KedroSession.create(env=self.env) as session:
+            self._model = session.run(pipeline_name=self.pipeline_name, node_names=['load_model']) 
+
+    @property
+    def project_name(self):
+        return self._project_name
+
+    @project_name.setter
+    def project_name(self, value):
+        self._project_name = value       
+
+    @property
+    def pipeline_name(self):
+        return self._pipeline_name
+
+    @pipeline_name.setter
+    def pipeline_name(self, value):
+        self._pipeline_name = value 
+
+    @property
+    def env(self):
+        return self._env
+
+    @env.setter
+    def env(self, value):
+        self._env = value 
 
     def predict(self, X, features_names=None):
         """
@@ -37,6 +66,6 @@ class LinearRegression(object):
         """
         logger.info("Predict called.")
         extra_params = json.loads(X)
-        with KedroSession.create(extra_params=extra_params) as session:
-            outputs = session.run(pipeline_name='predict', from_nodes=['parse_payload'], to_nodes=['convert_np_pd'])
+        with KedroSession.create(env=self.env, extra_params=extra_params) as session:
+            outputs = session.run(pipeline_name=self.pipeline_name, from_nodes=['parse_payload'], to_nodes=['convert_np_pd'])
             return outputs['pipeline_outputs']            
--- src/linear_regression/run_api_pipeline.py
+++ src/linear_regression/run_api_pipeline.py
@@ -3,11 +3,15 @@ from linear_regression.api_serving.linear_regression import LinearRegression
 import logging
 
 PAYLOAD = None
+ENV = None
 
 if len(sys.argv) > 1:
     PAYLOAD = sys.argv[1]
+    if len(sys.argv) > 2:
+        ENV = sys.argv[2]
 
 logging.getLogger().info('[Run API Pipeline]')
-LinearRegression().predict(X = PAYLOAD)
+api_pipeline = LinearRegression(env=ENV)
+api_pipeline.predict(X = PAYLOAD)
 logging.getLogger().info('[End API Pipeline]')
 
