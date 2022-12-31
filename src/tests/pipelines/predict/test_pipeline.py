d00fb67359fdb22d644ee2d117b2be063322ae48
sturiot <sturiot@outlook.fr>
2023-01-01 00:50:06 +0100

    fix tests

--- src/tests/pipelines/predict/test_pipeline.py
+++ src/tests/pipelines/predict/test_pipeline.py
@@ -1,11 +1,10 @@
-
 import pytest
 import numpy as np
 
 from linear_regression.api_serving.linear_regression import LinearRegression
 
 def test_pipeline():
-    inputs = np.array([[3, 5]])
+    inputs = "{\"ft_1\":3, \"ft_2\":5}"
     expected = 22.0
     actual = round(LinearRegression().predict(inputs)[0], 0)
     assert actual == expected
